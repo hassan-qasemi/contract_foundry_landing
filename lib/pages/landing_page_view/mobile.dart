f683796b58bd16b1b09ddb1bfe71b8bf253d10ef
h q <hssn.q.dev@gmail.com>
2025-05-31 09:14:05 -0600

    fix title

--- lib/pages/landing_page_view/mobile.dart
+++ lib/pages/landing_page_view/mobile.dart
@@ -51,7 +51,7 @@ class LandingPageMobile extends StatelessWidget {
                   Wrap(
                     children: [
                       Text(
-                        'Decentralized Business Interaction',
+                        'Trustless Digital Interaction',
                         textAlign: TextAlign.center,
                         style: context.displayLarge!.copyWith(
                             fontWeight: FontWeight.bold, color: Colors.white),
