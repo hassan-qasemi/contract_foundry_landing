9c131798aae0a033666f1ced881b7cac31f74440
h q <hssn.q.dev@gmail.com>
2025-06-01 10:57:38 -0600

    update litepaper link

--- lib/config.dart
+++ lib/config.dart
@@ -9,7 +9,7 @@ class Links {
   static final String emailLink = 'contractfoundry@outlook.com';
   // static final String mediumLink = 'https://medium.com/@contractfoundry/';
   static final String whitePaperLink =
-      'https://drive.google.com/file/d/1xrP4LZOZe8Lelz95WI9K98HKAqS_8Qwb/view?usp=sharing';
+      'https://drive.google.com/file/d/1_xKtHZovXEOdgmjb4NDCkRVABkuLbQ9B/view?usp=sharing';
   static final String documentsLink =
       'https://hassan-qasemi.github.io/contract_foundry_docs/';
   static final String appDownloadRepoAddress = '';
