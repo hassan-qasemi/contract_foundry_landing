ba6063e115bc329417c4587318740d80fc9ba680
h q <hssn.q.dev@gmail.com>
2025-06-04 23:10:08 -0600

    update icon and links

--- lib/config.dart
+++ lib/config.dart
@@ -9,9 +9,9 @@ class Links {
   static final String emailLink = 'contractfoundry@outlook.com';
   // static final String mediumLink = 'https://medium.com/@contractfoundry/';
   static final String whitePaperLink =
-      'https://drive.google.com/file/d/1px2CH1FGGaMtwUz409V2jtlXLuqmQW_h/view?usp=sharing';
+      'https://drive.google.com/file/d/1ycf6hzPThr4LPjMkJxRzHUa6CtmR-v7M/view?usp=sharing';
   static final String serviceprofLink =
-      'https://drive.google.com/file/d/1cDVZpAI0Y3Dz3Q0pVZLt3cggh68VsTTW/view?usp=sharing';
+      'https://drive.google.com/file/d/1RSCMHsyrcLw4jXCG5w21F0719_5XmkJ2/view?usp=sharing';
   static final String documentsLink =
       'https://hassan-qasemi.github.io/contract_foundry_docs/';
   static final String appDownloadRepoAddress = '';
