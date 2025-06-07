c25f50d1c70d919df373cf814ae2a39534b3bfb0
h q <hssn.q.dev@gmail.com>
2025-06-07 02:48:52 -0600

    update links

--- lib/config.dart
+++ lib/config.dart
@@ -9,9 +9,9 @@ class Links {
   static final String emailLink = 'contractfoundry@outlook.com';
   // static final String mediumLink = 'https://medium.com/@contractfoundry/';
   static final String whitePaperLink =
-      'https://drive.google.com/file/d/1ycf6hzPThr4LPjMkJxRzHUa6CtmR-v7M/view?usp=sharing';
+      'https://drive.google.com/file/d/1Oozol-l8gga0Zw0P7hgfutd1UzF8Snnz/view?usp=sharing';
   static final String serviceprofLink =
-      'https://drive.google.com/file/d/1RSCMHsyrcLw4jXCG5w21F0719_5XmkJ2/view?usp=sharing';
+      'https://drive.google.com/file/d/1HFZPQR83cffdy0ONu8vpDebWVakBasCb/view?usp=sharing';
   static final String documentsLink =
       'https://hassan-qasemi.github.io/contract_foundry_docs/';
   static final String appDownloadRepoAddress = '';
