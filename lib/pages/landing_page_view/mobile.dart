451af23e027a83aa833a3435495e78c48f23fc70
h q <hssn.q.dev@gmail.com>
2025-06-08 00:19:06 -0600

    fix mobile view

--- lib/pages/landing_page_view/mobile.dart
+++ lib/pages/landing_page_view/mobile.dart
@@ -16,22 +16,80 @@ class LandingPageMobile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        surfaceTintColor: Colors.transparent,
-        forceMaterialTransparency: true,
-        elevation: 0,
-        title: Text(
-          'Contract Foundry',
-          style: context.headlineSmall!
-              .copyWith(fontWeight: FontWeight.bold, color: Colors.white),
-        ),
-      ),
+          surfaceTintColor: Colors.transparent,
+          forceMaterialTransparency: true,
+          elevation: 0,
+          title: Text(
+            'Contract Foundry',
+            style: context.headlineSmall!
+                .copyWith(fontWeight: FontWeight.bold, color: Colors.white),
+          ),
+          leading: IconButton(
+              onPressed: () {
+                showModalBottomSheet(
+                    context: context,
+                    enableDrag: true,
+                    isDismissible: true,
+                    showDragHandle: true,
+                    builder: (c) => ListView(
+                          children: [
+                            ListTile(
+                              title: Text('Documents'),
+                              onTap: () async {
+                                await launchUrlString(Links.documentsLink);
+                              },
+                            ),
+                            ListTile(
+                              title: Text('Service portfolio & pricing'),
+                              onTap: () async {
+                                await launchUrlString(Links.serviceprofLink);
+                              },
+                            ),
+                            ListTile(
+                              title: Text('Get White paper'),
+                              onTap: () async {
+                                await launchUrlString(Links.whitePaperLink);
+                              },
+                            ),
+                          ],
+                        ));
+              },
+              icon: const Icon(Icons.sort))),
       body: Stack(
         children: [
           GlassmorphismBackground(isDarkMode: true),
           Center(
               child: PageView(
             scrollDirection: Axis.vertical,
-            children: [_HeadPage(), AboutPage()],
+            children: [
+              Container(
+                height: double.infinity,
+                width: double.infinity,
+                child: Center(
+                    child: Column(children: [
+                  const Expanded(child: SizedBox()),
+                  Wrap(
+                    children: [
+                      Text(
+                        'Decentralized\nTrustless\nInteractions',
+                        textAlign: TextAlign.center,
+                        style: context.displayLarge!.copyWith(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white70,
+                        ),
+                      )
+                    ],
+                  ),
+                  const Expanded(child: SizedBox()),
+                  const CopyrightWidget(),
+                  const SizedBox(
+                    height: 20,
+                  )
+                ])),
+              ),
+              _HeadPage(),
+              AboutPage()
+            ],
           )),
         ],
       ),
@@ -47,38 +105,25 @@ class _HeadPage extends StatelessWidget {
     final h = MediaQuery.of(context).size.height;
     final w = MediaQuery.of(context).size.width;
     return Container(
-      height: h,
-      width: w,
+      height: double.infinity,
+      width: double.infinity,
       padding: EdgeInsets.all(w * .01),
       color: Colors.transparent,
       child: Column(
         children: [
           const Expanded(child: SizedBox()),
-          Wrap(
-            children: [
-              Text(
-                'Decentralized Trustless Interactions',
-                textAlign: TextAlign.center,
-                style: context.displayLarge!
-                    .copyWith(fontWeight: FontWeight.bold, color: Colors.white),
-              )
-            ],
-          ),
-          SizedBox(height: 20),
           Padding(
-              padding: EdgeInsets.symmetric(horizontal: w * .2),
+              padding: EdgeInsets.symmetric(horizontal: 2),
               child: Wrap(
                 children: [
-                  Text(
-                      'Decentralized Secure business interactions by Blockchain Powered Self Sovereigh Identites and SLC\'s',
-                      textAlign: TextAlign.center,
+                  Text(_description,
                       style: context.bodyLarge!
                           .copyWith(color: context.primaryColor))
                 ],
               )),
           SizedBox(height: 20),
           Padding(
-              padding: EdgeInsets.symmetric(horizontal: w * .2),
+              padding: EdgeInsets.symmetric(horizontal: 2),
               child: Wrap(
                 children: [
                   Text('Launching Soon',
@@ -102,55 +147,29 @@ class _HeadPage extends StatelessWidget {
               const Expanded(child: SizedBox()),
             ],
           ),
-          const SizedBox(height: 20),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              const Expanded(child: SizedBox()),
-              TextButton(
-                  onPressed: () async {
-                    await launchUrlString(Links.whitePaperLink);
-                  },
-                  child: Text(
-                    'Get White paper',
-                  )),
-              SizedBox(
-                width: 4,
-              ),
-              TextButton(
-                  onPressed: () async {
-                    await launchUrlString(Links.documentsLink);
-                  },
-                  child: Text('Documents')),
-              const Expanded(child: SizedBox()),
-            ],
-          ),
-          const SizedBox(height: 5),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              const Expanded(child: SizedBox()),
-              TextButton(
-                  onPressed: () async {
-                    await launchUrlString(Links.serviceprofLink);
-                  },
-                  child: Text(
-                    'Service portfolio & pricing',
-                    style: context.bodyMedium!.copyWith(color: Colors.white),
-                  )),
-              const Expanded(child: SizedBox()),
-            ],
-          ),
           const Expanded(
             flex: 2,
             child: SizedBox(),
           ),
-          const CopyrightWidget(),
           const SizedBox(height: 20),
           const SocialLinksWidget(),
-          const Expanded(child: SizedBox()),
+          const SizedBox(height: 20),
         ],
       ),
     );
   }
+
+  final _description = '''
+Contract Foundry is a permissionless
+decentralized platform that provides self-
+sovereign identity, verifiable credentials, and
+trustless smart contracts as infrastructure.
+
+We empower users to manage their data,
+verify contracts and credentials securely,
+privately, and without intermediaries. Built on
+blockchain and Web3 principles, our mission
+is to redefine trust through automation,
+transparency, and control.
+''';
 }
