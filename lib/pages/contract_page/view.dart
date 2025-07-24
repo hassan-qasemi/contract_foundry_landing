0b89d7757b8825ba99fe034c59e6c9b72352fc17
h q <hssn.q.dev@gmail.com>
2025-07-24 10:15:11 -0600

    remove link loader future

--- lib/pages/contract_page/view.dart
+++ lib/pages/contract_page/view.dart
@@ -1,10 +1,8 @@
 import 'package:contract_foundry_landing_page/config.dart';
 import 'package:contract_foundry_landing_page/pages/landing_page_view/background.dart';
 import 'package:contract_foundry_landing_page/theme/theme_color_extension.dart';
-import 'package:contract_foundry_landing_page/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher_string.dart';
-import 'dart:html' as html;
 
 class ContractPageView extends StatelessWidget {
   final String contractDID;
@@ -12,61 +10,62 @@ class ContractPageView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: Future(() {
-          try {
-            checkDID(contractDID);
+    // return FutureBuilder(
+    //     future: Future(() {
+    //       try {
+    //         checkDID(contractDID);
 
-            // launchUrlString(
-            //     'https://contractfoundry.web.app/contract/$contractDID');
+    //         // launchUrlString(
+    //         //     'https://contractfoundry.web.app/contract/$contractDID');
 
-            final appLink =
-                'https://contractfoundry.web.app/contract/$contractDID';
-            html.window.location.href = appLink;
-          } catch (err) {
-            showDialog(
-                context: context,
-                builder: (c) => AlertDialog(
-                      content: ListTile(
-                        leading: const Icon(
-                          Icons.error_outline_outlined,
-                        ),
-                        title: Wrap(
-                          children: [Text(err.toString())],
-                        ),
-                      ),
-                    ));
-          }
-        }),
-        builder: (c, s) => Scaffold(
-              body: Container(
-                height: double.infinity,
-                width: double.infinity,
-                child: Stack(
-                  children: [
-                    GlassmorphismBackground(
-                      isDarkMode: true,
-                    ),
-                    Center(
-                        child: Column(children: [
-                      Expanded(child: SizedBox()),
-                      Icon(
-                        Icons.downloading_rounded,
-                        color: context.primaryContainer.withAlpha(190),
-                        size: 300,
-                      ),
-                      SizedBox(height: 30),
-                      TextButton.icon(
-                          onPressed: () async {
-                            await launchUrlString(Links.appDownloadRepoAddress);
-                          },
-                          label: Text('Download wallet app'),
-                          icon: Icon(Icons.download)),
-                      Expanded(child: SizedBox()),
-                    ]))
-                  ],
-                ),
+    //         final appLink =
+    //             'https://contractfoundry.web.app/contract/$contractDID';
+    //         html.window.location.href = appLink;
+    //       } catch (err) {
+    //         showDialog(
+    //             context: context,
+    //             builder: (c) => AlertDialog(
+    //                   content: ListTile(
+    //                     leading: const Icon(
+    //                       Icons.error_outline_outlined,
+    //                     ),
+    //                     title: Wrap(
+    //                       children: [Text(err.toString())],
+    //                     ),
+    //                   ),
+    //                 ));
+    //       }
+    //     }),
+    //     builder: (c, s) =>
+    return Scaffold(
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        child: Stack(
+          children: [
+            GlassmorphismBackground(
+              isDarkMode: true,
+            ),
+            Center(
+                child: Column(children: [
+              Expanded(child: SizedBox()),
+              Icon(
+                Icons.downloading_rounded,
+                color: context.primaryContainer.withAlpha(190),
+                size: 300,
               ),
-            ));
+              SizedBox(height: 30),
+              TextButton.icon(
+                  onPressed: () async {
+                    await launchUrlString(Links.appDownloadRepoAddress);
+                  },
+                  label: Text('Download wallet app'),
+                  icon: Icon(Icons.download)),
+              Expanded(child: SizedBox()),
+            ]))
+          ],
+        ),
+      ),
+    );
   }
 }
